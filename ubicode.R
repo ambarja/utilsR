ca82feafb8a92d9bd851b5e68efdba150333e7b8
Antony Barja <antony.barja8@gmail.com>
2022-02-01 10:08:54 -0500

    Update ubicode.R

--- ubicode.R
+++ ubicode.R
@@ -1,7 +1,17 @@
+# Ubigeo
 ubicode <- function(x){
   if(nchar(as.vector(x)) == 6){
     x <- sprintf("%s",x)
   }else{
     x <- sprintf("0%s",x)
+  }
+}
+
+# idmz
+ubicode <- function(x){
+  if(nchar(as.vector(x)) == 19){
+    x <- sprintf("%s",x)
+  }else{
+    x <- sprintf("%s0",x)
   }
 }
